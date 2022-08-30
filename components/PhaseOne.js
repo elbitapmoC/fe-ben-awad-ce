e8417341a61866d97b42147ba310940c2045df57
Ahkal <bp@sojourn.llc>
2022-08-30 17:58:12 -0400

    - Adds an ability to increment and decrement

--- components/PhaseOne.js
+++ components/PhaseOne.js
@@ -11,11 +11,20 @@ const PhaseOne = () => {
           setCount(count + 1);
         }}
         type="button"
-        class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        className="mt-8 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
       >
-        Incremembent By One
+        Increase By One
       </button>
-      ;
+
+      <button
+        onClick={() => {
+          setCount(count > 0 ? count - 1 : 0);
+        }}
+        type="button"
+        className="mt-8 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+      >
+        Decrease By One
+      </button>
     </>
   );
 };
