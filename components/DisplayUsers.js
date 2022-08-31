805b5f5eab39435496b10b9438497c9faab656b1
Ahkal <bp@sojourn.llc>
2022-08-31 10:02:06 -0400

    Completes final phase. - Appends new user info to array. - Styles user cards. - Dynamically loads in users - Pagination added

--- components/DisplayUsers.js
+++ components/DisplayUsers.js
@@ -1,16 +1,25 @@
 import React from "react";
 
 const DisplayUsers = ({ user }) => {
-  const { name, picture } = user;
+  const { name, picture, location } = user;
   return (
-    <li className="list-none flex-col mb-12">
-      <img
-        className="m-auto mb-4"
-        src={picture?.large}
-        alt={`Picture of ${name.first} ${name.last}`}
-      />
-      <p>{`${name.title}. ${name.first} ${name.last} `}</p>
-    </li>
+    <>
+      <div class="w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-8">
+        <div class="flex flex-col items-center pb-10 pt-10">
+          <img
+            class="mb-3 w-24 h-24 rounded-full shadow-lg"
+            src={picture?.large}
+            alt={`Picture of ${name.first} ${name.last}`}
+          />
+          <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+            {name.title}. {name.first} {name.last}
+          </h5>
+          <span class="text-sm text-gray-500 dark:text-gray-400">
+            {location.country}
+          </span>
+        </div>
+      </div>
+    </>
   );
 };
 
--- components/PhaseFinal.js
+++ components/PhaseFinal.js
@@ -3,23 +3,30 @@ import Loading from "./Loading";
 import DisplayUsers from "./DisplayUsers";
 
 const PhaseFinal = () => {
-  const [loading, setLoading] = useState(true);
+  const [nextPage, setNextPage] = useState(1);
   const [users, setUsers] = useState([]);
-  // Pagination
+  // Pagination - ?page=2
   // Append New User Info as we fetch new user info.
 
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch(
+        `https://randomuser.me/api?page=${nextPage}`
+      );
+
+      const results = await response.json();
+
+      // Appends NEW data to old data.
+      const newUserInfo = [...users, ...results.results];
+      setUsers(newUserInfo);
+      setNextPage(results.info.page + 1);
+    } catch (err) {
+      console.error(`Bugs...🐛: ${err}`);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://randomuser.me/api");
-        const results = await response.json();
-        setUsers(results);
-      } catch (err) {
-        console.error(`Bugs...🐛: ${err}`);
-        setLoading(true);
-      }
-    };
-    fetchData();
+    fetchUserData();
   }, []);
 
   if (users.length <= 0)
@@ -34,14 +41,12 @@ const PhaseFinal = () => {
 
   return (
     <>
-      <ul>
-        {users.results?.map((user, idx) => (
-          <DisplayUsers key={idx} user={user} />
-        ))}
-      </ul>
+      {users.map((user, idx) => (
+        <DisplayUsers key={idx} user={user} />
+      ))}
       <button
         onClick={() => {
-          console.log("clicked");
+          fetchUserData();
         }}
         type="button"
         className="mt-8 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
