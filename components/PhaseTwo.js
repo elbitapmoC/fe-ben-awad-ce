3c9d9dcc0223505170311f41eb8eba58578cc1d9
Ahkal <bp@sojourn.llc>
2022-10-20 23:52:36 -0400

    We'll stick to async await

--- components/PhaseTwo.js
+++ components/PhaseTwo.js
@@ -13,7 +13,7 @@ const PhaseTwo = () => {
 
   // 2. Display as a string the info sent in from API.
   // If you want to use the result from the asynchronous code, you should do so inside the fetchAndLogAsyncAwait function, not outside.
-  // METHOD 1 | Async Await ------------------------------------ START
+  // Async Await ------------------------------------ START
   const fetchAndLogAsyncAwait = async () => {
     try {
       const response = await fetch("https://randomuser.me/api");
@@ -25,23 +25,11 @@ const PhaseTwo = () => {
       console.error(err);
     }
   };
-  // METHOD 1 | Async Await ------------------------------------ END
-
-  // METHOD 2 | Promises ------------------------------------ START
-  // const fetchAndLogPromise = () => {
-  //   fetch("https://randomuser.me/api")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       let dataString = JSON.stringify(data);
-  //       setData(dataString);
-  //     });
-  //   console.error(err);
-  // };
-  // METHOD 2 | Promises ------------------------------------ END
+  // Async Await ------------------------------------ END
 
   useEffect(() => {
     fetchAndLogAsyncAwait() // make sure to catch any error
-      .catch(console.error("🤧 The C19 Bug is worse than this bug."));
+      .catch(console.error("🤧 C19 is worse."));
   }, []);
   return (
     <>
